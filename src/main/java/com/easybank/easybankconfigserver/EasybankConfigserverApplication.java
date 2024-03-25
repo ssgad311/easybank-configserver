adf9917d5dfbfd560c67131e614b3ea6b68b0046
GaddamS <saikumar.gaddam@proagrica.com>
2024-03-25 21:53:48 +0530

    Created application properties files for easybank

--- src/main/java/com/easybank/easybankconfigserver/EasybankConfigserverApplication.java
+++ src/main/java/com/easybank/easybankconfigserver/EasybankConfigserverApplication.java
@@ -2,8 +2,10 @@ package com.easybank.easybankconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class EasybankConfigserverApplication {
 
 	public static void main(String[] args) {

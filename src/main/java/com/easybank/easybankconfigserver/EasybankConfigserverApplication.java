064641cbc1effbfbdfe0a38cf5d8c8d8c7c6efed
GaddamS <saikumar.gaddam@proagrica.com>
2024-03-25 20:51:38 +0530

    Initial commit

---
+++ src/main/java/com/easybank/easybankconfigserver/EasybankConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.easybank.easybankconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybankConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybankConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easybank/easybankconfigserver/EasybankConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybank.easybankconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybankConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
